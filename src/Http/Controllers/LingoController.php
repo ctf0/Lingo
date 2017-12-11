3680236d2c40a957dbc599378131f04b79dc1398
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2017-12-11 20:38:01 +0200

    fix defaults

--- src/Http/Controllers/LingoController.php
+++ src/Http/Controllers/LingoController.php
@@ -25,7 +25,7 @@ class LingoController extends Controller
 
     public function index()
     {
-        return view('Lingo::lingo-' . config('lingo.theme'));
+        return view('Lingo::lingo-' . config('lingo.theme', 'bulma'));
     }
 
 
